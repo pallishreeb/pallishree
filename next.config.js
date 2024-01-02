320113573a1ab5572b7ad0c6dddc52cbd1f31044
Pallishree <pallishreebehera01@gmail.com>
2024-01-02 23:23:51 +0530

    Add CDN

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
-
-const isProd = true
 const nextConfig = {
     output:"export",
-    images:{unoptimized: true},
-    assetPrefix: isProd ? 'https://cdn.statically.io/gh/pallishreeb/pallishree/gh-pages/' : '',
-
+    images:{unoptimized: true}
+    
 }
 
 module.exports = nextConfig
